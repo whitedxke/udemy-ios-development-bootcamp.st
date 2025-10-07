8763ca3b935f2100bd307a29ae380046bff816fa
White Dxke <whitedxke@gmail.com>
2025-10-07 19:30:13 +0300

    Restarting the Bootcamp Repository (Building Projects from Scratch).

---
+++ Development/Xylophone/Xylophone/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Pavel Betenya on 7.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
