f32e16b0a3edb2f5ad526ef28645e2fcf2a0a9ec
White Dxke <whitedxke@gmail.com>
2025-10-22 15:40:56 +0300

    Сreated a project, and initialized it.

---
+++ Development/9-10. iOS App Design Patterns, and Structuring Module/Quizzler/Quizzler/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Pavel Betenya on 22.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
