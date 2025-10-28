474b844da0324a068446008a43d3a1f281b68392
White Dxke <whitedxke@gmail.com>
2025-10-28 23:24:21 +0300

    Created a new project for challenge after theory.

---
+++ Development/9-10. iOS App Design Patterns, and Structuring Module/Life Line/Life Line/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Life Line
+//
+//  Created by Pavel Betenya on 28.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
