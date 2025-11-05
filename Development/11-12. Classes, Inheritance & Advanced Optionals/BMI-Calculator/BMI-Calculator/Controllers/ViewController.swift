ecf2e883fd6e9945b95b733c3977214132d6d7f3
White Dxke <whitedxke@gmail.com>
2025-11-05 23:10:45 +0300

    Advanced Swift Programming - Classes, Inheritance & Advanced Optionals.

---
+++ Development/11-12. Classes, Inheritance & Advanced Optionals/BMI-Calculator/BMI-Calculator/Controllers/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  BMI-Calculator
+//
+//  Created by Pavel Betenya on 5.11.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
