d29f43c88591ec2a93b9aa9558892036ab778a83
White Dxke <whitedxke@gmail.com>
2025-11-18 00:34:21 +0300

    Created a start project to continue studies.

---
+++ Development/13-14. Networking, JSON Parsing, APIs and Core Location/Clima/Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Pavel Betenya on 18.11.25.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+    @IBOutlet weak var conditionImageView: UIImageView!
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
