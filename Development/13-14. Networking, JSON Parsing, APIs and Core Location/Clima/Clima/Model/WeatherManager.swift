49742a1d1dc8cd2e37c7c8b2c4d522167536837b
White Dxke <whitedxke@gmail.com>
2025-11-25 23:34:02 +0300

    Refactored code.

--- Development/13-14. Networking, JSON Parsing, APIs and Core Location/Clima/Clima/Model/WeatherManager.swift
+++ Development/13-14. Networking, JSON Parsing, APIs and Core Location/Clima/Clima/Model/WeatherManager.swift
@@ -11,15 +11,13 @@ struct WeatherManager {
     let apiKey = "openweathermap-api-key"
     
     let domain = "https://api.openweathermap.org"
+    
     let path = "/data/2.5/weather"
+    
     let parameter = "units=metric"
     
-    var weatherURL: String {
-        "\(domain)\(path)?\(parameter)&appid=\(apiKey)"
-    }
-    
     func getWeather(city: String) {
-        let url = "\(weatherURL)&q=\(city)"
+        let url = "\(domain)\(path)?appid=\(apiKey)&q=\(city)&\(parameter)"
         performRequest(url: url)
     }
     
