f15b0d4ae47a2438a8f78d28b81a92f244cf0f13
White Dxke <whitedxke@gmail.com>
2025-12-09 22:30:07 +0300

    Complex design and layouts using SwiftUI.

---
+++ Development/16. SwiftUI and Declarative Programming/PersonalCard/PersonalCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PersonalCard
+//
+//  Created by Pavel Betenya on 9.12.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
