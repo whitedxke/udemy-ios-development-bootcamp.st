8cd98e04f49adfba2311198811a75b8b6ccda3ed
White Dxke <whitedxke@gmail.com>
2025-12-10 17:00:34 +0300

    Complex design and layouts using SwiftUI.

--- Development/16. SwiftUI and Declarative Programming/PersonalCard/PersonalCard/ContentView.swift
+++ Development/16. SwiftUI and Declarative Programming/PersonalCard/PersonalCard/ContentView.swift
@@ -9,16 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.black).edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("Photo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("Pavel Betenya")
+                    .font(Font.custom("Pacifico", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("Mobile Engineer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 20))
+                Divider().frame(height: 4)
+                InformationView(
+                    text: "XXX (XX) XXX-XX-XX",
+                    image: "phone.fill",
+                )
+                Divider().frame(height: 4)
+                InformationView(
+                    text: "username@example.com",
+                    image: "envelope.fill",
+                )
+            }
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
