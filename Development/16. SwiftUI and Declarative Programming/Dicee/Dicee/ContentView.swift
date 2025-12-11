4e35e785f6e0a81ea1fd4a6c97b88df2be410da2
White Dxke <whitedxke@gmail.com>
2025-12-11 16:04:35 +0300

    Building in Functionality, and Managing State.

--- Development/16. SwiftUI and Declarative Programming/Dicee/Dicee/ContentView.swift
+++ Development/16. SwiftUI and Declarative Programming/Dicee/Dicee/ContentView.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("Background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("Logotype")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 24))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .frame(width: 150, height: 60)
+                        .background(Color.red)
+                        .cornerRadius(10)
+                }
+                .fixedSize()
+            }
         }
-        .padding()
+    }
+}
+
+struct DiceView: View {
+    let n: Int
+    
+    var body: some View {
+        Image("Dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
     }
 }
